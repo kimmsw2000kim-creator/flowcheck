0cbb0c66bdf71faff83a04c1beb0735248348370
Ppori <kimmsw2000kim@gmail.com>
2026-07-09 10:30:10 +0900

    fix) adminpage

--- frontend/src/pages/admin/AdminPage.tsx
+++ frontend/src/pages/admin/AdminPage.tsx
@@ -4,7 +4,7 @@ import UserManagementTab from './UserManagementTab';
 function AdminPage () {
     return (
         <section className="admin-page">
-            
+            <h1>관리자 페이지</h1>
         </section>
     )
 }
